76cef1b6384e19ad087917332415990c479a774d
Robel Anbessie <robitesf@gmail.com>
2026-01-22 13:06:19 +0300

    add vercel

--- backend/server.js
+++ backend/server.js
@@ -37,3 +37,5 @@ app.post('/api/admin/super-login', authSuperAdmin);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+module.exports = app;
