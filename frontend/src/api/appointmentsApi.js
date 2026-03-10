60ffa339dd79ad006d93a61f6851cfe6f9187057
Robel Anbessie <robitesf@gmail.com>
2026-03-10 11:55:03 +0300

    edit landing page

--- frontend/src/api/appointmentsApi.js
+++ frontend/src/api/appointmentsApi.js
@@ -6,7 +6,6 @@ import apiClient from '../api'; // ማዕከላዊ የሆነውን apiClient እ
 export const createNewAppointment = (appointmentData) => apiClient.post('/appointments', appointmentData);
 export const updateAppointment = (id, updatedData) => apiClient.put(`/appointments/${id}`, updatedData);
 export const deleteAppointment = (id) => apiClient.delete(`/appointments/${id}`);
-export const getAllAppointments = () => apiClient.get('/appointments');
 
 // ሁሉንም ቀጠሮዎች ለማምጣት (ለአድሚን ዳሽቦርድ)
 export const fetchAppointments = () => apiClient.get('/appointments');
